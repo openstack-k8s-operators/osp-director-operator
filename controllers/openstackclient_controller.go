ca4c3dae8b8cb4658c3e7278b1ca650bc00e1610
Martin Schuppert <mschuppert@redhat.com>
2020-12-17 15:32:32 +0000

    Fix openstack client env merge
    This resulted in unwanted openstackclient restarts.

--- controllers/openstackclient_controller.go
+++ controllers/openstackclient_controller.go
@@ -171,7 +171,7 @@ func (r *OpenStackClientReconciler) podCreateOrUpdate(instance *ospdirectorv1bet
 	op, err := controllerutil.CreateOrUpdate(context.TODO(), r.Client, pod, func() error {
 		// HostAliases
 		pod.Spec.HostAliases = instance.Spec.HostAliases
-		pod.Spec.Containers[0].Env = common.MergeEnvs([]corev1.EnvVar{}, envVars)
+		pod.Spec.Containers[0].Env = common.MergeEnvs(pod.Spec.Containers[0].Env, envVars)
 
 		// labels
 		common.InitMap(&pod.Labels)
