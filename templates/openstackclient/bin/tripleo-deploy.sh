7e9895d324a164134bd3ef50b690a90a497134d7
Martin Schuppert <mschuppert@redhat.com>
2021-01-21 13:06:08 +0000

    Use pvc for for cloud-admin home directory
    Move deployment operations to cloud-admin user and use a pvc as
    the home directory to have everything in /hob/cloud-admin persistent.
    
    Also use pvc for /etc/hosts to have entries added by the tripleo
    deployment persistent.

--- templates/openstackclient/bin/tripleo-deploy.sh
+++ templates/openstackclient/bin/tripleo-deploy.sh
@@ -2,15 +2,16 @@
 
 set -eux
 
-sed -i "/# We only get here if no errors/a \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ rc=0" /usr/lib/python3.6/site-packages/tripleoclient/v1/tripleo_deploy.py
-sed -i "s/clouds_home_dir = .*/clouds_home_dir = os.path.expanduser('~')/" /usr/lib/python3.6/site-packages/tripleoclient/utils.py
+# in case of --output-only no rc is set when successful
+sudo sed -i "/# We only get here if no errors/a \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ rc=0" /usr/lib/python3.6/site-packages/tripleoclient/v1/tripleo_deploy.py
 # disable running dhcp on all interfaces, setting disable_configure_safe_defaults in the interface template does not work
-sed -i '/^set -eux/a disable_configure_safe_defaults=true' /usr/share/openstack-tripleo-heat-templates/network/scripts/run-os-net-config.sh
+sudo sed -i '/^set -eux/a disable_configure_safe_defaults=true' /usr/share/openstack-tripleo-heat-templates/network/scripts/run-os-net-config.sh
 
+mkdir -p ~/tripleo-deploy
 rm -rf ~/tripleo-deploy/overcloud-ansible*
 unset OS_CLOUD
 
-openstack tripleo deploy \
+sudo openstack tripleo deploy \
     --templates /usr/share/openstack-tripleo-heat-templates \
     -r /usr/share/openstack-tripleo-heat-templates/roles_data.yaml \
     -n /usr/share/openstack-tripleo-heat-templates/network_data.yaml \
@@ -34,10 +35,11 @@ cd ~/tripleo-deploy
 output_dir=$(ls -dtr overcloud-ansible-* | tail -1)
 ln -sf ${output_dir} overcloud-ansible
 cd ${output_dir}
+# we run with --standalone, therefore have to remove transport=local from ansible.cfg
 sed -i '/transport/d' ansible.cfg
-sed -i "/blockinfile/a \ \ \ \ unsafe_writes: yes" /usr/share/ansible/roles/tripleo-hosts-entries/tasks/main.yml
 
-# change ansible_ssh_user to cloud-admin, todo deployment-user should be also set in the inventroy as ansible_ssh_user
+# For standalone role tripleo_deploy sets root for the ansible_ssh_user in the inventory.
+# Change it to cloud-admin
 sed -i 's/ansible_ssh_user: root/ansible_ssh_user: cloud-admin/g' ~/tripleo-deploy/overcloud-ansible/inventory.yaml
 
 time ansible-playbook -i inventory.yaml --become deploy_steps_playbook.yaml
