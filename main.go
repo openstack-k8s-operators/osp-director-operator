e69fd4023552a456209b557c748aa4bf1a1d42cd
Dan Prince <dprince@redhat.com>
2021-08-18 15:52:34 -0400

    Generate OpenStackConfigVersion API/controller.

--- main.go
+++ main.go
@@ -263,6 +263,14 @@ func main() {
 		}
 	}
 
+	if err = (&controllers.OpenStackConfigVersionReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("OpenStackConfigVersion"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "OpenStackConfigVersion")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
