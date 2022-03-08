88f5e408834e289d39836408e6e4e9c2faf0a93b
Dan Prince <dprince@redhat.com>
2022-03-08 09:34:59 -0500

    Cleanups to scripts/customize_staged_images.sh

--- scripts/customize_staged_images.sh
+++ scripts/customize_staged_images.sh
@@ -1,20 +1,11 @@
+# Script to modify the image references in the staged image bundle so they can be
+# installed easily tested from the registry-proxy.engineering.redhat.com staging repo internally.
 set -xe
 
 VERSION=${1:-"0.0.1"}
 REPO=${2:-"quay.io/openstack-k8s-operators"}
-MY_INDEX_IMG="$REPO/osp-director-operator-index:$VERSION"
-MY_BUNDLE_IMG="$REPO/osp-director-operator-bundle:$VERSION"
-
-#example: docker://registry-proxy.engineering.redhat.com/rh-osbs/rhosp-rhel8-tech-preview-osp-director-provisioner:1.2.2}
-PROVISIONER_IMG=${PROVISIONER_IMG:?"Please set the PROVISIONER_IMG"}
-DOWNLOADER_IMG=${DOWNLOADER_IMG:?"Please set the DOWNLOADER_IMG"}
-
-# example: docker://registry-proxy.engineering.redhat.com/rh-osbs/rhosp-rhel8-tech-preview-osp-director-downloader:1.2.2
-DOWNLOADER_IMG=${DOWNLOADER_IMG:?"Please set the DOWNLOADER_IMG"}
-
-# example: docker://registry-proxy.engineering.redhat.com/rh-osbs/rhosp-rhel8-tech-preview-osp-director-operator:1.2.2
-OPERATOR_IMG=docker://registry-proxy.engineering.redhat.com/rh-osbs/rhosp-rhel8-tech-preview-osp-director-operator:1.2.2
-OPERATOR_IMG=${OPERATOR_IMG:?"Please set the OPERATOR_IMG"}
+INDEX_IMG="$REPO/osp-director-operator-index:$VERSION"
+NEW_BUNDLE_IMG="$REPO/osp-director-operator-bundle:$VERSION"
 
 # example: docker://registry-proxy.engineering.redhat.com/rh-osbs/rhosp-rhel8-tech-preview-osp-director-operator-bundle:1.2.2-4
 BUNDLE_IMG=${BUNDLE_IMG:?"Please set the BUNDLE_IMG"}
@@ -33,18 +24,18 @@ podman unshare bash $WORK_DIR/copy-file.sh
 echo $WORK_DIR/osp-director-operator.clusterserviceversion.yaml
 
 # modify this CSV so that it contains "staged" images so we can test it
-
 sed -e "s|registry.redhat.io/rhosp-rhel8-tech-preview/|registry-proxy.engineering.redhat.com/rh-osbs/rhosp-rhel8-tech-preview-|" -i $WORK_DIR/osp-director-operator.clusterserviceversion.yaml
 
-
+# build a new "staged" bundle image. THIS ONE IS FOR INTERNAL STAGED TESTING ONLY.
 cat > $WORK_DIR/Dockerfile << EOF_CAT
 FROM $BUNDLE_IMG
 COPY osp-director-operator.clusterserviceversion.yaml /manifests/osp-director-operator.clusterserviceversion.yaml
 EOF_CAT
 
 UPDATED_BUNDLE_IMG=$(podman build -q $WORK_DIR)
-podman tag $UPDATED_BUNDLE_IMG $MY_BUNDLE_IMG
-podman push $MY_BUNDLE_IMG
+podman tag $UPDATED_BUNDLE_IMG $NEW_BUNDLE_IMG
+podman push $NEW_BUNDLE_IMG
 
-opm index add --bundles ${MY_BUNDLE_IMG} --tag ${MY_INDEX_IMG} -u podman --pull-tool podman
-podman push ${MY_INDEX_IMG}
+# Create a new index for testing the "staged" bundle image built above
+opm index add --bundles ${NEW_BUNDLE_IMG} --tag ${INDEX_IMG} -u podman --pull-tool podman
+podman push ${INDEX_IMG}
