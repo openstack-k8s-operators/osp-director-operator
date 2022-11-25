aff25ea917b42049ee604e95ddb10f6bfa082074
Martin Schuppert <mschuppert@redhat.com>
2022-11-25 13:24:37 +0100

    cleanup osnetcfg status if there are no reservations in any ctlplane network

--- controllers/openstacknetconfig_controller.go
+++ controllers/openstacknetconfig_controller.go
@@ -299,6 +299,8 @@ func (r *OpenStackNetConfigReconciler) Reconcile(ctx context.Context, req ctrl.R
 	//
 	instance.Status.ProvisioningStatus.NetDesiredCount = r.getNetDesiredCount(instance.Spec.Networks)
 	instance.Status.ProvisioningStatus.NetReadyCount = 0
+
+	ctlplaneReservations := map[string]int{}
 	for _, net := range instance.Spec.Networks {
 
 		// TODO: (mschuppert) cleanup single removed netConfig in list
@@ -331,8 +333,25 @@ func (r *OpenStackNetConfigReconciler) Reconcile(ctx context.Context, req ctrl.R
 				return ctrlResult, nil
 			}
 
+			if net.IsControlPlane {
+				ctlplaneReservations[osNet.Spec.NameLower] = osNet.Status.ReservedIPCount
+			}
+
 			instance.Status.ProvisioningStatus.NetReadyCount++
 		}
+	}
+
+	// all nodes have a ctlplane network, if there are no reservations
+	// in any of the ctlplane subnets, reset the osnetcfg host reservation status
+	statusHostReservationCleanup := true
+	for _, res := range ctlplaneReservations {
+		if res > 0 {
+			statusHostReservationCleanup = false
+			break
+		}
+	}
+	if statusHostReservationCleanup {
+		instance.Status.Hosts = map[string]ospdirectorv1beta1.OpenStackHostStatus{}
 	}
 
 	//
@@ -1266,7 +1285,6 @@ func (r *OpenStackNetConfigReconciler) ensureIPReservation(
 				reservation.Deleted = true
 				osNet.Spec.RoleReservations[role].Reservations[idx] = reservation
 			}
-
 		}
 	}
 
@@ -1421,7 +1439,6 @@ func (r *OpenStackNetConfigReconciler) ensureIPs(
 				reservations = append(reservations, oldReservation)
 			}
 		}
-
 	}
 
 	//
