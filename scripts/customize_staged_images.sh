dca38fce55ddac511b409471ca8c5ee22b317c89
Martin Schuppert <mschuppert@redhat.com>
2025-03-13 08:16:05 +0100

    bump to golang 1.24
    Signed-off-by: Martin Schuppert <mschuppert@redhat.com>

--- scripts/customize_staged_images.sh
+++ scripts/customize_staged_images.sh
@@ -19,7 +19,7 @@ WORK_DIR=$(mktemp -d)
 cat > $WORK_DIR/Dockerfile << EOF_CAT
 FROM $BUNDLE_IMG as bundle
 
-FROM golang:1.21 AS editor
+FROM golang:1.23 AS editor
 COPY --from=bundle /manifests/osp-director-operator.clusterserviceversion.yaml /osp-director-operator.clusterserviceversion.yaml
 
 # (mschuppert) with new build process (konflux) and proper config of ImageContentSourcePolicy we no longer need those replaces,
